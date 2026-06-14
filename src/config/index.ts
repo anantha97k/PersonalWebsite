84e3ea4a795d304855128985c311cbb9d4d79c84
Civic <131238075+new-trainee5108@users.noreply.github.com>
2026-06-14 11:44:41 +0800

    Updating the website to match the resume

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,36 +3,48 @@ import type { SiteConfig, SiteContent } from "../types";
 export const SITE_CONFIG: SiteConfig = {
   title: "Kumar Portfolio",
   author: "Kumar",
-  description:
-    "Software Engineer",
+  description: "Software Engineer",
   lang: "en",
   siteLogo: "/ak-pic.png",
   navLinks: [
+    { text: "Experience", href: "#experience" },
     { text: "Education", href: "#education" },
     { text: "Projects", href: "#projects" },
-    { text: "Experience", href: "#experience" },
     { text: "About", href: "#about" },
   ],
   socialLinks: [
     { text: "LinkedIn", href: "https://www.linkedin.com/in/ananthakumar28" },
     { text: "Github", href: "https://github.com/anantha97k" },
   ],
-
 };
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
     name: "Ananthakumar [Kumar]",
-    specialty: "FullStack Developer",
-    summary:
-      `Full-Stack Software Engineer with expertise in Python, FastAPI, React.js, and cloud technologies.
+    specialty: "Backend / FullStack Developer",
+    summary: `Full-Stack Software Engineer with skillsl in Python, FastAPI, React.js, and cloud technologies.
        Experienced in building RESTful APIs, implementing asynchronous processing systems, and
-       deploying scalable applications using AWS, Redis, and PostgreSQL.`,
+       deploying scalable applications using AWS, Redis, and PostgreSQL`,
   },
+  experience: [
+    {
+      company: "Train & Place Up-skilling Program",
+      position: "Software Engineering Track",
+      startDate: "USAINS Holding Sdn.Bhd.",
+      endDate: "",
+      summary: [
+      `Completed 3-month intensive full-stack web development program, applying SDLC and
+        Agile/Scrum methodologies in collaborative team projects using JavaScript, Python, SQL, MySQL,
+        and Git version control`,
+      `Developed professional competencies in technical communication, problem-solving, and change
+        management through hands-on projects and structured training modules`,
+      ],
+    },
+  ],
   education: [
     {
-      company: "",
-      position: "Skills",
+      company: "Technical Skills",
+      position: "",
       startDate: "",
       endDate: "",
       summary: [
@@ -40,31 +52,38 @@ export const SITE_CONTENT: SiteContent = {
         "Frameworks & Libraries: React.js, Redux, Node.js, Express.js, FastAPI, Tailwind CSS, Celery",
         "Backend Technologies: RESTful APIs, WebSockets, JWT Authentication, Redis, Message Queues, Asynchronous Processing, Microservices",
         "Cloud & DevOps: AWS, Docker, CI/CD, Git, GitHub",
-        "Databases: PostgreSQL, MySQL, MongoDB, SQLite "
+        "Databases: PostgreSQL, MySQL, MongoDB, SQLite ",
       ],
     },
     {
       company: "",
-      position: "Bac. Hons Electronic and Communications Engineering, CGPA: 3.02",
-      startDate: "Oct 2018 - Jan 2023,",
+      position:
+        "Bac. Hons Electronic and Communications Engineering, CGPA: 3.02",
+      startDate: "2018 - 2023,",
       endDate: "Sg. Long",
       summary: [
         `Courses such as Wireless Communications, Object-Oriented Programming,
           Procedural Programming, Computer Architecture and Assembly Programming etc.`,
-          `Final Year Project : Performance Analysis of MIMO Communication Systems using MATLAB & simulink`,
-          `Developed and implemented models of MIMO Communication Systems
+        `Final Year Project : Performance Analysis of MIMO Communication Systems using MATLAB & simulink`,
+        `Developed and implemented models of MIMO Communication Systems
           to analyse various Modulation and Encoding schemes along with Diversity techniques.`,
-          `Visualized the simulated data using graphs to demonstrate the bit error rate (BER)
-          performance of different MIMO models`
+        `Visualized the simulated data using graphs to demonstrate the bit error rate (BER)
+          performance of different MIMO models`,
       ],
     },
   ],
   projects: [
     {
       name: "Anime Search App",
-      summary: "Built a two-page React + TypeScript SPA with Redux-managed state, and instant search.",
+      summary:
+        "Built a two-page React + TypeScript SPA with Redux-managed state, and instant search.",
       skills: [
-        'React', 'Typescript', 'react-router', 'Redux', 'Shadcn UI', 'Tailwind',
+        "React",
+        "Typescript",
+        "react-router",
+        "Redux",
+        "Shadcn UI",
+        "Tailwind",
       ],
       linkSource: "https://github.com/anantha97k/AnimeSearch-App",
       image: "/SearchApp.png",
@@ -73,52 +92,56 @@ export const SITE_CONTENT: SiteContent = {
       name: "Data processing",
       summary: `Built a CSV upload system, processing files asynchronously
                 and notify users upon completion in realtime.`,
-      skills: ['Python', 'FastAPI', 'SQLite', 'Redis', 'Python RQ', 'React'],
+      skills: ["Python", "FastAPI", "SQLite", "Redis", "Python RQ", "React"],
       linkSource: "https://github.com/anantha97k/CSV-Processing",
       image: "/CSV.png",
     },
     {
       name: "Todo-a-List",
-      summary: "Developed a Todo List application using the Express framework, with the MVC architecture",
-      skills: ['NodeJS', 'Prisma ORM', 'Docker'],
+      summary:
+        "Developed a Todo List application using the Express framework, with the MVC architecture",
+      skills: ["NodeJS", "Prisma ORM", "Docker"],
       linkSource: "https://github.com/anantha97k/todo-list",
       image: "/todos.png",
     },
     {
       name: "Resume Maker",
       summary: "Enables users to create, view, update, and manage resumes",
-      skills: ['React', 'react-router', 'Express', 'PostgreSQL', 'MongoDB', 'Render Deployment'],
+      skills: [
+        "React",
+        "react-router",
+        "Express",
+        "PostgreSQL",
+        "MongoDB",
+        "Render Deployment",
+      ],
       linkSource: "https://github.com/anantha97k/monorepo",
       image: "/Resume.png",
     },
     {
-      name: "Image Processing Service",
-      summary: `Developed a Image Processing application for simple demonstration to illustrate skills in caching, AWS, asynchronous processing`,
-      skills: ['Python', 'FastAPI', 'JWT', 'AWS S3', 'CloudFront', 'Redis', 'Celery'],
+      name: "Image Processing",
+      summary: `Developed an Image Processing app to illustrate skills in caching, AWS and asynchronous processing`,
+      skills: [
+        "Python",
+        "FastAPI",
+        "JWT",
+        "AWS S3",
+        "CloudFront",
+        "Redis",
+        "Celery",
+      ],
       linkSource: "https://github.com/anantha97k/Image-Processing",
       image: "/image-processing.png",
     },
   ],
-  experience : [
-    {
-      company: "Train & Place Up-skilling Program",
-      position: "Software Engineering Track",
-      startDate: "USAINS Holding Sdn.Bhd.",
-      endDate: "",
-      summary: [
-        `Technical Courses such as Statistics for Data Mining, JavaScript, Software Development
-and Testing, Database Access with SQL, Website Development with HTML5 and CSS3, etc.`,
-        `Soft skills Courses such as Change Management, Technical Communication &
-Presentation Skills, etc.`,
-      ],
-  },
-],
 
   about: {
     description: [
-      `I am an aspiring Software Engineer and Full-Stack Developer with a strong foundation for building scalable, user-focused applications. With hands-on experience from a structured Software Engineering upskilling program, I combine analytical problem-solving with practical coding expertise. I am comfortable working across both frontend and backend systems.`,
+      `My projects demonstrate a commitment to reliability, real-time systems, and maintainable code. I'm passionate about delivering solutions that 
+        work efficiently in production environments.`,
 
-      `I have applied my skills to end-to-end projects that demonstrate performance, reliability, and clean architecture.These experiences have strengthened my focus on intelligent automation, real-time systems, and maintainable code, and I am excited to contribute to dynamic development projects.`
-    ]
+        `Email: anantha97k@gmail.com`,
+        `Tel: +60 17 698 0784`
+    ],
   },
 };
